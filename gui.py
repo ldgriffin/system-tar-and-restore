118019b498c8a9bb2fa3ba86db3840e42ba1d7fc
Panagiotis Mavrogiorgos <pmav99@gmail.com>
2013-10-12 00:12:33 +0300

    Add cross-version compatibility imports.

--- gui.py
+++ gui.py
@@ -6,3 +6,20 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
+# Standard Library imports
+import sys
+
+# cross-version compatibility
+PY_VERSION = sys.version_info.major
+if PY_VERSION < 3:
+    import Tkinter as tk
+    import ttk
+    from tkFileDialog import askdirectory as tkaskdirectory
+    from ScrolledText import ScrolledText as tkScrolledText
+else:
+    import tkinter as tk
+    from tkinter import ttk
+    from tkinter.filedialog import askdirectory as tkaskdirectory
+    from tkinter.scrolledtext import ScrolledText as tkScrolledText
+
+
