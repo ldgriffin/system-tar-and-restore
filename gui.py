c20a8674dcdec89cb2e95d587b973fc4ba1e6131
Panagiotis Mavrogiorgos <pmav99@gmail.com>
2013-10-26 20:13:17 +0300

    Check for sudo existence. If it does not exist, use "su -c".

--- gui.py
+++ gui.py
@@ -127,7 +127,14 @@ class NotebookTab(ttk.Frame, FormLayoutMixin):
         raise NotImplementedError("You must implemement in subclasses!")
 
     def cb_execute_command(self, *args, **kwargs):
-        command = "xterm -hold -e sudo %s -i cli -q" % self.command.get()
+        # check for sudo existence. If it does not exist, use "su -c"
+        try:
+            subprocess.check_call(["which", "sudo"], stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            command = "xterm -hold -e su -c '%s -i cli -q'" % self.command.get()
+        else:
+            command = "xterm -hold -e sudo %s -i cli -q" % self.command.get()
+
         subprocess.call(shlex.split(command))
 
         ## Use local lookups to improve performance
